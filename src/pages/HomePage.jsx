28c5c29ad927eb2f9fb57aeef5079e9a0bc57cb7
IvanSitnikov1 <iw.sitnikoff@yandex.ru>
2024-02-18 10:33:13 +0300

    настроены стили

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,8 +7,6 @@ export const HomePage = () => {
       <h1 className="text-xl font-medium text-black mb-5">
         Welcome to Books App
       </h1>
-      <p className="text-slate-500 mb-10">Learn new skills! </p>
-
       <LinkButton to={ROUTES.books} title="View Books" />
     </div>
   );
